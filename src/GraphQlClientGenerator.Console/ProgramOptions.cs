0fc14651fe0de43b22b1f8207a3e0f24a12e6b0e
Husqvik <husqvik@hotmail.com>
2020-04-26 15:08:11 +0200

    tweaks

--- src/GraphQlClientGenerator.Console/ProgramOptions.cs
+++ src/GraphQlClientGenerator.Console/ProgramOptions.cs
@@ -10,22 +10,22 @@ namespace GraphQlClientGenerator.Console
         [Option('o', "outputFileName", Required = true, HelpText = "Output file name")]
         public string OutputFileName { get; set; }
 
-        [Option('n', "namespace", Required = true, HelpText = "Root namespace")]
+        [Option('n', "namespace", Required = true, HelpText = "Root namespace all classes and other members are generated to")]
         public string Namespace { get; set; }
 
         [Option("authorization", Required = false, HelpText = "Authorization header value")]
         public string Authorization { get; set; }
 
+        [Option("classPostfix", Required = false, HelpText = "Class postfix, for instance for version control; value \"V2\" extends class name to \"TypeNameV2\"")]
+        public string ClassPostfix { get; set; }
+
         [Option("csharpVersion", Required = false, HelpText = "C# version compatibility; allowed values: " + nameof(CSharpVersion.Compatible) + " (default), " + nameof(CSharpVersion.Newest) + ", " + nameof(CSharpVersion.NewestWithNullableReferences))]
         public CSharpVersion CSharpVersion { get; set; }
 
-        [Option("classPostfix", Required = false, HelpText = "Class postfix")]
-        public string ClassPostfix { get; set; }
+        [Option("memberAccessibility", Required = false, HelpText = "Class and interface access level; allowed values: " + nameof(MemberAccessibility.Public) + " (default), " + nameof(MemberAccessibility.Internal))]
+        public MemberAccessibility MemberAccessibility { get; set; }
 
         [Option("partialClasses", Required = false, HelpText = "Mark classes as \"partial\"")]
         public bool PartialClasses { get; set; }
-
-        [Option("memberAccessibility", Required = false, HelpText = "Class and interface access level; allowed values: " + nameof(MemberAccessibility.Public) + " (default), " + nameof(MemberAccessibility.Internal))]
-        public MemberAccessibility MemberAccessibility { get; set; }
     }
 }
