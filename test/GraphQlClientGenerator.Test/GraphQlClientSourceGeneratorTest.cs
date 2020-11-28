ddaf03bb31374c7e5b532673020b7123253d9285
Husqvik <husqvik@hotmail.com>
2020-11-29 00:09:29 +0100

    include new configuration property in the test

--- test/GraphQlClientGenerator.Test/GraphQlClientSourceGeneratorTest.cs
+++ test/GraphQlClientGenerator.Test/GraphQlClientSourceGeneratorTest.cs
@@ -56,7 +56,8 @@ namespace GraphQlClientGenerator.Test
                             { "build_property.GraphQlClientGenerator_IdTypeMapping", "String" },
                             { "build_property.GraphQlClientGenerator_JsonPropertyGeneration", "Always" },
                             { "build_property.GraphQlClientGenerator_CustomClassMapping", "Query:Tibber|RootMutation:TibberMutation Consumption:ConsumptionEntry;Production:ProductionEntry" },
-                            { "build_property.GraphQlClientGenerator_Headers", "Authorization:Basic XXX|X-REQUEST-ID:123456789" }
+                            { "build_property.GraphQlClientGenerator_Headers", "Authorization:Basic XXX|X-REQUEST-ID:123456789" },
+                            { "build_property.GraphQlClientGenerator_ScalarFieldTypeMappingProvider", "GraphQlClientGenerator.DefaultScalarFieldTypeMappingProvider, GraphQlClientGenerator" }
                         }));
 
             var compilation = CompilationHelper.CreateCompilation(null, "SourceGeneratorTestAssembly");
