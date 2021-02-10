7f29c8429de6af0715c481c254c4a1c8975688f5
Simon Cropp <simon.cropp@gmail.com>
2021-02-10 17:08:25 +1100

    use a top level method

--- src/GraphQlClientGenerator.Console/Program.cs
+++ src/GraphQlClientGenerator.Console/Program.cs
@@ -1,44 +1,36 @@
 ﻿using System;
 using System.Threading.Tasks;
 using CommandLine;
+using GraphQlClientGenerator.Console;
 
-namespace GraphQlClientGenerator.Console
+if (TryParseArguments(args, out var options))
+    await GenerateGraphQlClientSourceCode(options);
+else
+    Environment.Exit(1);
+
+static bool TryParseArguments(string[] args, out ProgramOptions options)
 {
-    internal class Program
+    options = null;
+
+    var parserResult = Parser.Default.ParseArguments<ProgramOptions>(args);
+    if (parserResult.Tag == ParserResultType.NotParsed)
+        return false;
+
+    options = parserResult.MapResult(o => o, null);
+    return true;
+}
+
+static async Task GenerateGraphQlClientSourceCode(ProgramOptions options)
+{
+    try
     {
-        public static async Task Main(string[] args)
-        {
-            if (TryParseArguments(args, out var options))
-                await GenerateGraphQlClientSourceCode(options);
-            else
-                Environment.Exit(1);
-        }
-
-        private static bool TryParseArguments(string[] args, out ProgramOptions options)
-        {
-            options = null;
-
-            var parserResult = Parser.Default.ParseArguments<ProgramOptions>(args);
-            if (parserResult.Tag == ParserResultType.NotParsed)
-                return false;
-
-            options = parserResult.MapResult(o => o, null);
-            return true;
-        }
-
-        private static async Task GenerateGraphQlClientSourceCode(ProgramOptions options)
-        {
-            try
-            {
-                var files = await GraphQlCSharpFileHelper.GenerateClientSourceCode(options);
-                foreach (var file in files)
-                    System.Console.WriteLine($"File {file.FullName} generated successfully ({file.Length:N0} B). ");
-            }
-            catch (Exception exception)
-            {
-                System.Console.WriteLine($"An error occurred: {exception}");
-                Environment.Exit(2);
-            }
-        }
+        var files = await GraphQlCSharpFileHelper.GenerateClientSourceCode(options);
+        foreach (var file in files)
+            System.Console.WriteLine($"File {file.FullName} generated successfully ({file.Length:N0} B). ");
+    }
+    catch (Exception exception)
+    {
+        System.Console.WriteLine($"An error occurred: {exception}");
+        Environment.Exit(2);
     }
 }
