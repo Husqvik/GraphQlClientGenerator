e1f28c8a889382f2d49a9ed9b8e33960099d8ae3
Artur Gordashnikov <gao.artur@gmail.com>
2023-01-27 20:00:35 +0200

    Add missing cli options (#1)
    Co-authored-by: Artur Gordashnikov <arturg@accessfintech.com>

--- src/GraphQlClientGenerator.Console/GraphQlCSharpFileHelper.cs
+++ src/GraphQlClientGenerator.Console/GraphQlCSharpFileHelper.cs
@@ -53,8 +53,11 @@ internal static class GraphQlCSharpFileHelper
                 MemberAccessibility = options.MemberAccessibility,
                 IdTypeMapping = options.IdTypeMapping,
                 FloatTypeMapping = options.FloatTypeMapping,
+                IntegerTypeMapping = options.IntegerTypeMapping,
+                BooleanTypeMapping = options.BooleanTypeMapping,
                 JsonPropertyGeneration = options.JsonPropertyAttribute,
-                EnumValueNaming = options.EnumValueNaming
+                EnumValueNaming = options.EnumValueNaming,
+                IncludeDeprecatedFields = options.IncludeDeprecatedFields
             };
 
         if (!KeyValueParameterParser.TryGetCustomClassMapping(options.ClassMapping, out var customMapping, out var customMappingParsingErrorMessage))
